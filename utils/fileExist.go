30d685181a4e720e9ada6e57646a4314e34112b0
Stef <sstefin@bk.ru>
2020-10-15 02:58:00 -0500

    Added func for file checking

---
+++ utils/fileExist.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+    "os"
+)
+
+func FileExists(name string) bool {
+    if _, err := os.Stat(name); err != nil {
+        if os.IsNotExist(err) {
+            return false
+        }
+    }
+    return true
+}
